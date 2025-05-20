2865f19146379ccf0dc131d5ac8e54dcbfabcf1c
Tonny Zhao <thelonelyotaku315@gmail.com>
2025-05-19 21:48:47 -0400

    Add Contact and Projects sections with templates, styles, and tests; update app layout to include new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { AboutMeSectionComponent } from './components/about-me-section/about-me-section.component';
-
+import { ContactSectionComponent } from './components/contact-section/contact-section.component';
+import { ProjectsSectionComponent } from './components/projects-section/projects-section.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [AboutMeSectionComponent, NavigationBarComponent, CommonModule, RouterOutlet]
+  imports: [
+    AboutMeSectionComponent,
+    NavigationBarComponent,
+    CommonModule,
+    ContactSectionComponent,
+    ProjectsSectionComponent,
+  ],
 })
 export class AppComponent {
   title = 'personal-website';
