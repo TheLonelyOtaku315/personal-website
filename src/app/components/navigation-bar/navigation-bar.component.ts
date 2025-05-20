f33a5d240e4801ec47512646f9f90b67f8e6529c
Tonny Zhao <thelonelyotaku315@gmail.com>
2025-05-19 22:21:08 -0400

    Refactor theme management: remove unused SVG background, update theme styles, and implement dark/light mode toggle in navigation bar

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-navigation-bar',
   imports: [],
   templateUrl: './navigation-bar.component.html',
-  styleUrl: './navigation-bar.component.css'
+  styleUrl: './navigation-bar.component.css',
 })
 export class NavigationBarComponent {
+  isDarkMode = false;
 
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const theme = localStorage.getItem('theme');
+      if (theme === 'dark') {
+        this.setDarkMode(true);
+      } else {
+        this.setDarkMode(false);
+      }
+    } else {
+      this.setDarkMode(false);
+    }
+  }
+
+  toggleTheme(event: Event) {
+    this.setDarkMode((event.target as HTMLInputElement).checked);
+  }
+
+  setDarkMode(isDark: boolean) {
+    this.isDarkMode = isDark;
+    if (isDark) {
+      this.renderer.removeClass(this.document.body, 'light-theme');
+      this.renderer.addClass(this.document.body, 'dark-theme');
+      if (typeof window !== 'undefined' && window.localStorage) {
+        localStorage.setItem('theme', 'dark');
+      }
+    } else {
+      this.renderer.removeClass(this.document.body, 'dark-theme');
+      this.renderer.addClass(this.document.body, 'light-theme');
+      if (typeof window !== 'undefined' && window.localStorage) {
+        localStorage.setItem('theme', 'light');
+      }
+    }
+  }
 }
