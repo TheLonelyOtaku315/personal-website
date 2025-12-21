21cf6767b41ed24e2e22ce6d1b65b9f9bd93e815
Tonny Zhao <thelonelyotaku315@gmail.com>
2025-12-21 13:24:34 -0500

    Clean up CSS files and remove unused styles

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 1467, hash: '22bb9e53e75de74feea190c29d342230edfb6937f012c83094678b07dddedefb', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1198, hash: 'cda6b1bf4b0e297344a0129785d6a86ef96abfb210b44847daab891078ef6d1d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 71405, hash: 'fa31d917f9fb9fc419b2a21da13e185db99c72cb9ec6500b1050713479bb9410', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1467, hash: '23ba56d1b7f37f8ab85dc69471efd56a1b39f5b4d10f7418fb887571a0ff3352', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1198, hash: 'f8eea603562ea3d3352b9655dcfa54a04ff4b4e61b23c07072679ebf8d6e9b89', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 67434, hash: '8fee88d4297f08e5404c081d782e2e8d31714a2ab4fa00de817702b60f8a89ae', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-N6TYELC2.css': {size: 1565, hash: 'm+ckXwmULZo', text: () => import('./assets-chunks/styles-N6TYELC2_css.mjs').then(m => m.default)}
   },
 };
