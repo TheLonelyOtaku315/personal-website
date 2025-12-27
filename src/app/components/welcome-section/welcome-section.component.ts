ce7e917940e16c4d518b34e069bf92bfc3990e00
Tonny Zhao <thelonelyotaku315@gmail.com>
2025-12-27 11:33:06 -0500

    feat: enhance welcome section with multilingual support and translations for greeting, intro, and resume button

--- src/app/components/welcome-section/welcome-section.component.ts
+++ src/app/components/welcome-section/welcome-section.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-welcome-section',
   templateUrl: './welcome-section.component.html',
   styleUrls: ['./welcome-section.component.css'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
 })
 export class WelcomeSectionComponent {
 // download button animation is 4s
